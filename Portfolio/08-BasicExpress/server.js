58ddae148aaeb6cbd67817cc4135cfc348e6e004
EduardoAzuul <0260522@up.edu.mx>
2025-10-05 13:44:57 -0600

    08 - Basic Express

---
+++ Portfolio/08-BasicExpress/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+app.get('/calculate-bmi',(req, res) => {
+  const weight = parseFloat(req.query.weight);
+  const height = parseFloat(req.query.height);
+
+  console.log('Received weight:', weight, 'height:', height);
+
+  if (isNaN(weight) || isNaN(height) || height <= 0) {
+    return res.status(400).send('Invalid input');
+  }
+  const bmi = weight / ((height / 100) ** 2);
+  res.send(`Your BMI is ${bmi.toFixed(2)}`);
+ 
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
